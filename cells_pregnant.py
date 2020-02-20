f3fbc6c26012c5ab65552688f0d2cc57f38cdecf
s0nghuiming <s0nghuiming@hotmail.com>
2020-02-20 16:42:10 +0800

    Spawn algorithm thinking

--- cells_pregnant.py
+++ cells_pregnant.py
@@ -8,7 +8,7 @@ from cell import Cell
 WINDOW_WIDTH = 800
 WINDOW_HEIGHT = 600
 CELL_WIDTH = 4
-LIMIT_NUM = 500
+LIMIT_NUM = 5000
 
 
 # Data
@@ -21,7 +21,7 @@ SUM_CELL = CELL_INIT_NUM
 
 
 # Flags
-PAUSE = False
+PAUSE = False # False and True. Default is True.
 
 
 # Cell chain init
@@ -66,7 +66,7 @@ for x, y in zip(X, Y):
         search_map[x] = []
         search_map[x].append(y)
 
-# Function
+# Common function
 
 # time_update
 #   update text of time label
@@ -75,10 +75,24 @@ def time_update():
     curtime = time.strftime('%H:%M:%S')
     label_time.config(text=curtime)
 
+
+# Function
+
 # spawn:
 #    One alive cell spawns his son.
 #    cell: alive cell,
 #    speed: how many sons born in one spawn,
+#    Description: || 占位符
+#    |生育新细胞的条件比较苛刻。最好是不要生在其他细胞上，也就是不重合；
+#    |按照|delta_map||的定义，是一个|3*3|的矩阵的中心为父亲细胞，外围八个
+#    |点作为子细胞。按这个方法简化的话，所有的细胞都应该出现在等距离的点
+#    |阵中。所以，要把细胞初始化的代码做修改，改为在矩阵的点上生成初始点。
+#    |另外，在本函数中，目前的判断条件是子细胞不能超出边框，而且新子细胞
+#    |不能和已有细胞重合。如果不满足条件，则返回空。这样的话此次函数调用
+#    |就不产生新细胞，需要等待下一轮调用，而如果下一轮也如此轮空的话，那么
+#    |很不幸，需要等更多的时间了。这种情况发生的概率将随着细胞的数量变大而
+#    |变大。如何能解决这种时间的浪费呢？
+#    |引入卷积，将能解决这个问题。接下来介绍……
 def spawn(cell=None, speed=1):
     if None != cell:
         xs = cell.x
@@ -106,33 +120,49 @@ def spawn(cell=None, speed=1):
 
 def one_generation():
     global SUM_CELL
+    global PAUSE
     loop_id = None
     loop_id = root.after(500, one_generation)
-    # Modify data as you wish
-    if len(cell_chain) < LIMIT_NUM:
-        father_id = np.random.choice(list(cell_chain))
-        spawn_cells = spawn(cell_chain[father_id])
-        #canvas.delete(tk.ALL)
-        for s_c in spawn_cells:
-            rect_id = canvas.create_rectangle(s_c.x, 
-                                          s_c.y, 
-                                          s_c.x+CELL_WIDTH, 
-                                          s_c.y+CELL_WIDTH)
-            s_c.serialno = rect_id
-            cell_chain[rect_id] = s_c
-            SUM_CELL = SUM_CELL + 1
-        if 0 != len(spawn_cells):
-            pass
-        label_sum.config(text="Total cell: " + str(len(cell_chain)))
-    else:
-        print("Maximum exceeds.")
-        root.after_cancel(loop_id)
 
-    root.update()
+    if True == PAUSE:
+        if loop_id:
+            root.after_cancel(loop_id)
+        root.update()
+    else:
+        # Modify data as you wish
+        if len(cell_chain) < LIMIT_NUM:
+            father_id = np.random.choice(list(cell_chain))
+            spawn_cells = spawn(cell_chain[father_id])
+            #canvas.delete(tk.ALL)
+            for s_c in spawn_cells:
+                rect_id = canvas.create_rectangle(s_c.x, 
+                                              s_c.y, 
+                                              s_c.x+CELL_WIDTH, 
+                                              s_c.y+CELL_WIDTH)
+                s_c.serialno = rect_id
+                cell_chain[rect_id] = s_c
+                SUM_CELL = SUM_CELL + 1
+            if 0 != len(spawn_cells):
+                pass
+            label_sum.config(text="Total cell: " + str(len(cell_chain)))
+        else:
+            print("Maximum exceeds.")
+            root.after_cancel(loop_id)
+
+        root.update()
 
 # Pause not impl
 def set_pause(event):
-    print("Paused")
+    global PAUSE
+    if False == PAUSE:
+        PAUSE = True
+        btn_pause.config(text='Resume')
+        print("Paused")
+    else:
+        PAUSE = False
+        btn_pause.config(text='Pause')
+        root.after(0, one_generation)
+        print("Resumed")
 
 btn_pause.bind("<Button-1>", set_pause)
 
